28c1da7ccc898823f0e5260734a5031a58980d58
DangerN00dl3 <53065305+DangerN00dl3@users.noreply.github.com>
2020-04-28 16:12:38 -0400

    Add files via upload
    Basic JTT, with moron AI and simply GUI but fully functional

--- JTTMain.java
+++ JTTMain.java
@@ -2,92 +2,71 @@ import javax.swing.*;
 import java.awt.event.*;
 import java.awt.*;
 
-class JTTMain extends JFrame implements ActionListener
-{
-	public boolean gameWon() { return false; }
-	public boolean gameLost() { return false; }
-	public boolean gameTie() { return false; }
-	
-	class Closer extends WindowAdapter
-	{
-		public void windowClosing(WindowEvent e)
-		{
+class JTTMain extends JFrame implements ActionListener {
+	class Closer extends WindowAdapter {
+		public void windowClosing(WindowEvent e) {
 			System.out.println("Hope you had fun!");
 			System.exit(0);
 		}
 	}
-	
-	class Node
-	{
+
+	class Node {
 		Color color;
 		int size;
-		int x,y;
-		
-		public Node(int x, int y, Color c, int s) 
-			{ this.x=x; this.y=y; color=c; size=s; }
-			
-		public void changeColor()
-		{
-			color=new Color(
-				(int)(150),
-				(int)(50),
-				(int)(50));
+		int x, y;
+
+		public Node(int x, int y, Color c, int s) {
+			this.x = x;
+			this.y = y;
+			color = c;
+			size = s;
 		}
-		
-		public void draw(Graphics g)
-		{
+
+		public void changeColor() {
+			color = new Color((int) (150), (int) (50), (int) (50));
+		}
+
+		// Function to change the nodes selected by the CPU
+		public void changeColorCPU() {
+			color = new Color((int) (50), (int) (255), (int) (0));
+		}
+
+		public void draw(Graphics g) {
 			g.setColor(color);
-			g.fillOval(x,y,size, size/2);
+			g.fillOval(x, y, size, size / 2);
 		}
 	}
-	class Backdrop extends JPanel
-	{		
-		Node [] nodes;
-		
-		public Backdrop()
-		{
-			nodes=new Node[9];
-			setSize(1100,600);
-			//f=new Node(100,100, Color.yellow, 200);
-			for(int i=0; i<nodes.length; i++)
-			{
-				if(i<=2)
-				{
-					nodes[i]=new Node(
-						(int)(125+350*i),
-						(int)(100),
-						Color.yellow,
-						(int)(100)
-					);
+
+	class Backdrop extends JPanel {
+		Node[] nodes;
+
+		public Backdrop() {
+			nodes = new Node[9];
+			setSize(1100, 600);
+			// f=new Node(100,100, Color.yellow, 200);
+			for (int i = 0; i < nodes.length; i++) {
+				if (i <= 2) {
+					nodes[i] = new Node((int) (125 + 350 * i), (int) (100), Color.yellow, (int) (100));
+				} else if (i <= 5) {
+					nodes[i] = new Node((int) (300 + 175 * (i - 3)), (int) (250), Color.yellow, (int) (100));
+				} else if (i <= 8) {
+					nodes[i] = new Node((int) (125 + 350 * (i - 6)), (int) (400), Color.yellow, (int) (100));
 				}
-				else if(i<=5)
-				{
-					nodes[i]=new Node(
-						(int)(300+175*(i-3)),
-						(int)(250),
-						Color.yellow,
-						(int)(100)
-					);
-				}
-				else if(i<=8)
-				{
-					nodes[i]=new Node(
-						(int)(125+350*(i-6)),
-						(int)(400),
-						Color.yellow,
-						(int)(100)
-					);
-				}	
 			}
 		}
-		
-		public void setcolor(int i) { nodes[i].changeColor(); }
 
-		public void paintComponent(Graphics g)
-		{
+		public void setcolor(int i) {
+			nodes[i].changeColor();
+		}
+
+		public void setcolorCPU(int i) {
+			nodes[i].changeColorCPU();
+		}
+
+		public void paintComponent(Graphics g) {
 			// gameboard
 			g.setColor(Color.gray);
-			g.fillRect(0,0, 1100,600);
+			g.fillRect(0, 0, 1100, 600);
 			// lines
 			g.setColor(Color.black);
 			g.drawLine(175, 125, 875, 125);
@@ -99,23 +78,23 @@ class JTTMain extends JFrame implements ActionListener
 			g.drawLine(875, 125, 175, 125);
 			g.drawLine(875, 125, 175, 425);
 			g.drawLine(875, 125, 525, 425);
-			//f.draw(g);
-			for(int i=0; i<nodes.length; i++)
+			// f.draw(g);
+			for (int i = 0; i < nodes.length; i++)
 				nodes[i].draw(g);
 			// numbers
 			g.setColor(Color.black);
-			g.drawString("1",172,125);
-			g.drawString("2",522,125);
-			g.drawString("3",872,125);
-			g.drawString("4",347,275);
-			g.drawString("5",522,275);
-			g.drawString("6",697,275);
-			g.drawString("7",172,425);
-			g.drawString("8",522,425);
-			g.drawString("9",872,425);
+			g.drawString("1", 172, 125);
+			g.drawString("2", 522, 125);
+			g.drawString("3", 872, 125);
+			g.drawString("4", 347, 275);
+			g.drawString("5", 522, 275);
+			g.drawString("6", 697, 275);
+			g.drawString("7", 172, 425);
+			g.drawString("8", 522, 425);
+			g.drawString("9", 872, 425);
 		}
 	}
-	
+
 	JLabel header;
 	JLabel instruction;
 	JButton setcolor;
@@ -129,97 +108,110 @@ class JTTMain extends JFrame implements ActionListener
 	boolean win;
 	boolean lose;
 	boolean draw;
-	
-	public void actionPerformed(ActionEvent e)
-	{
-		if(e.getSource()==setcolor)
-		{
-			String x=input.getText();
-			int i=Integer.parseInt(x);
-			gameboard.setcolor(i-1);
+
+	public void actionPerformed(ActionEvent e) {
+		if (e.getSource() == setcolor) {
+			String x = input.getText();
+			int i = Integer.parseInt(x);
+			b[i] = 1;
+			gameboard.setcolor(i - 1);
 			// forces a redraw of the gameboard art object
 			gameboard.repaint();
-		}
-		else if(e.getSource()==playagain)
-		{
-			JTTMain submarine=new JTTMain();
-		}
-		else if(e.getSource()==exit)
-		{
+			CPUturn();
+		} else if (e.getSource() == playagain) {
+			JTTMain submarine = new JTTMain();
+		} else if (e.getSource() == exit) {
 			System.out.println("Hope you had fun!");
 			System.exit(0);
 		}
 	}
-	
-	public JTTMain()
-	{
+
+	// Board Work
+	int[] b = new int[10];
+
+	public void CPUturn() {
+		if (gameWon() == true) {
+			return;
+		}
+		for (int i = 1; i < 10; i++) {
+			if (b[i] == 0) {
+				b[i] = 2;
+				gameboard.setcolorCPU(i - 1);
+				gameboard.repaint();
+				gameLost();
+				return;
+			}
+		}
+		System.out.println("It's a tie");
+		System.exit(0);
+	}
+
+	public boolean gameWon() {
+		if ((b[1] == 1 && b[2] == 1 && b[3] == 1) || (b[1] == 1 && b[4] == 1 && b[8] == 1)
+				|| (b[1] == 1 && b[5] == 1 && b[9] == 1) || (b[2] == 1 && b[4] == 1 && b[7] == 1)
+				|| (b[2] == 1 && b[6] == 1 && b[9] == 1) || (b[3] == 1 && b[5] == 1 && b[7] == 1)
+				|| (b[3] == 1 && b[6] == 1 && b[8] == 1) || (b[4] == 1 && b[5] == 1 && b[6] == 1)
+				|| (b[7] == 1 && b[8] == 1 && b[9] == 1)) {
+			System.out.println("You Win");
+			System.exit(0);
+			return true;
+		} else
+			return false;
+	}
+
+	public void gameLost() {
+		if ((b[1] == 2 && b[2] == 2 && b[3] == 2) || (b[1] == 2 && b[4] == 2 && b[8] == 2)
+				|| (b[1] == 2 && b[5] == 2 && b[9] == 2) || (b[2] == 2 && b[4] == 2 && b[7] == 2)
+				|| (b[2] == 2 && b[6] == 2 && b[9] == 2) || (b[3] == 2 && b[5] == 2 && b[7] == 2)
+				|| (b[3] == 2 && b[6] == 2 && b[8] == 2) || (b[4] == 2 && b[5] == 2 && b[6] == 2)
+				|| (b[7] == 2 && b[8] == 2 && b[9] == 2)) {
+			System.out.println("You Lose");
+			System.exit(0);
+		}
+	}
+
+	public JTTMain() {
 		setTitle("Jerry Tac Toe: Tik Tac Toe but better!");
-		setSize(1100,700);
+		setSize(1100, 700);
 		addWindowListener(new Closer());
-		
-		header=new JLabel("Welcome to Jerry Tac Toe! Have fun playing. But be" +
-							" warned: you are no match for Jerry!");
-		instruction=new JLabel("Enter the node you'd like to mark then press \"Submit Move\".");
-		setcolor=new JButton("   Submit Move   ");
-		input=new JTextField("");
-		youwon=new JLabel("You won! Jerry bows down to you. Click the left button to play again or right to exit.");
-		youlost=new JLabel("You lost! Jerry laughs in your face. Click the left button to play again or right to exit.");
-		tie=new JLabel("The game has ended in a tie! Jerry has shown you mercy just this once. Look left to play again or right to exit.");
-		playagain=new JButton("Play Again/Reset");
-		exit=new JButton("  Exit  ");
-		gameboard=new Backdrop();
-		
+
+		header = new JLabel(
+				"Welcome to Jerry Tac Toe! Have fun playing. But be" + " warned: you are no match for Jerry!");
+		instruction = new JLabel("Enter the node you'd like to mark then press \"Submit Move\".");
+		setcolor = new JButton("   Submit Move   ");
+		input = new JTextField("");
+		playagain = new JButton("Play Again/Reset");
+		exit = new JButton(" Exit ");
+		gameboard = new Backdrop();
+
 		setcolor.addActionListener(this);
 		playagain.addActionListener(this);
 		exit.addActionListener(this);
-		
-		// put stuff in the window
-		Container glass=getContentPane();
-		glass.setLayout( new BorderLayout() ) ; // layout manager
 
-		JPanel bottom=new JPanel();
+		// put stuff in the window
+		Container glass = getContentPane();
+		glass.setLayout(new BorderLayout()); // layout manager
+
+		JPanel bottom = new JPanel();
 		bottom.setLayout(new BorderLayout());
-		bottom.add(instruction,"West");
-		bottom.add(input,"Center");
-		bottom.add(setcolor,"East");
-		
-		JPanel top=new JPanel();
+		bottom.add(instruction, "West");
+		bottom.add(input, "Center");
+		bottom.add(setcolor, "East");
+
+		JPanel top = new JPanel();
 		top.setLayout(new BorderLayout());
-		top.add(header,"Center");
-		
-		if(gameWon()==true)
-		{
-			top.add(youwon, "Center");
-			top.add(playagain,"West");
-			top.add(exit,"East");
-		}
-		
-		else if(gameLost()==true)
-		{
-			top.add(youlost, "Center");
-			top.add(playagain,"West");
-			top.add(exit,"East");
-			
-		}
-		
-		else if(gameTie()==true)
-		{
-			top.add(tie, "Center");
-			top.add(playagain,"West");
-			top.add(exit,"East");
-			
-		}
-		
+		top.add(header, "Center");
+
 		glass.add(top, "North");
 		glass.add(gameboard, "Center");
 		glass.add(bottom, "South");
-		glass.add(new JPanel(),"East");
-		glass.add(new JPanel(),"West");
-		
+		glass.add(new JPanel(), "East");
+		glass.add(new JPanel(), "West");
+
 		setVisible(true);
 	}
-	public static void main(String [] args)
-	{
-		JTTMain submarine=new JTTMain();
+
+	public static void main(String[] args) {
+		JTTMain submarine = new JTTMain();
 	}
 }
